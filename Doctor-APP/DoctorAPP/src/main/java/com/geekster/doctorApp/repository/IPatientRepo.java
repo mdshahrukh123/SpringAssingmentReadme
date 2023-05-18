99185129aee6b1daf5839d6f9263d14f96fe0363
shahrukh <mohdshahrukhmzn1@gmail.com>
2023-05-18 23:27:30 +0530

    remove

--- Doctor-APP/DoctorAPP/src/main/java/com/geekster/doctorApp/repository/IPatientRepo.java
+++
@@ -1,9 +0,0 @@
-package com.geekster.doctorApp.repository;
-
-import com.geekster.doctorApp.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IPatientRepo extends JpaRepository<Patient, Long> {
-
-    Patient findFirstByPatientEmail(String userEmail);
-}
